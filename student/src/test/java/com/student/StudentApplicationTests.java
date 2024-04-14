0a39e65a6a355ac93b0a0b9b647ac2e80306cc2f
gayathri2227 <166359575+gayathri2227@users.noreply.github.com>
2024-04-14 17:37:53 +0530

    Update StudentApplicationTests.java

--- student/src/test/java/com/student/StudentApplicationTests.java
+++ student/src/test/java/com/student/StudentApplicationTests.java
@@ -1,13 +1,17 @@
 package com.student;
 
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+ 
 @SpringBootTest
 class StudentApplicationTests {
-
+ 
 	@Test
 	void contextLoads() {
+		 assertTrue(true);
 	}
-
+ 
 }
