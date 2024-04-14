ef139bec64f5d68fda1fcc430dc87fb3a7d4c7de
gayathri2227 <166359575+gayathri2227@users.noreply.github.com>
2024-04-14 16:10:19 +0530

    Update StudentRepository.java

--- student/src/main/java/com/student/repository/StudentRepository.java
+++ student/src/main/java/com/student/repository/StudentRepository.java
@@ -1,8 +1,4 @@
 package com.student.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
