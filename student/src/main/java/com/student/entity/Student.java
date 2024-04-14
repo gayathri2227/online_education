6156b656271c3ba3c2f1a28330bf896ccea0aed2
gayathri2227 <166359575+gayathri2227@users.noreply.github.com>
2024-04-14 16:09:30 +0530

    Update Student.java

--- student/src/main/java/com/student/entity/Student.java
+++ student/src/main/java/com/student/entity/Student.java
@@ -6,8 +6,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -165,7 +163,7 @@ public class Student {
 
 
 	public Student() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 
